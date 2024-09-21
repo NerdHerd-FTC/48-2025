f57d3e621f8f53d20dee0ff4776e0cddb53d7d94
Anay Saran <anaysaran@gmail.com>
2024-09-20 18:06:20 -0700

    Add swing arm code
    Added moveArm method, which accepts a double between 0 and 1 for position.
    
    Co-Authored-By: Anthony W. <contact@tonnieboy.com>
    Co-Authored-By: Jesus-Lopezz <179983180+Jesus-Lopezz@users.noreply.github.com>
    Co-Authored-By: James-108 <181036572+James-108@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/swingArm.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Swing Arm")
+public class swingArm extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+        DcMotor arm = hardwareMap.dcMotor.get("arm");
+
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //TODO: Set this uwu
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        telemetry.addLine("ready to go");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up && !gamepad1.dpad_down && !gamepad1.dpad_left && !gamepad1.dpad_right){
+                moveArm(1.0, arm);
+            }
+            if ((gamepad1.dpad_left || gamepad1.dpad_right) && !gamepad1.dpad_up && !gamepad1.dpad_down){
+                moveArm(0.5, arm);
+            }
+            if (gamepad1.dpad_down && !gamepad1.dpad_up && !gamepad1.dpad_left && !gamepad1.dpad_right){
+                moveArm(0.0, arm);
+            }
+            telemetry.addLine("yippee! the robot is working!");
+            telemetry.update();
+        }
+    }
+
+    public void moveArm(double pos, DcMotor arm){
+        final int UPPER_BOUND = 300;
+
+        int targetPosition = (int) (pos * UPPER_BOUND);
+
+        arm.setTargetPosition(targetPosition);
+    }
+}
