b8d55877f2a3bea4fa928bc0406cc142476ab231
Tonnieboy300 <tonnieboy300@icloud.com>
2024-10-29 22:54:16 -0700

    Add robot direction settings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumDriveFO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumDriveFO.java
@@ -20,8 +20,14 @@ public class mecanumDriveFO extends LinearOpMode {
 
         double driverDirection = 0;
         String driverLocation = "Audience";
+        double robotOffset = 0;
+        double robotHeading = 0;
+        String robotDirection = "Forward";
         telemetry.addData("Driver Location", driverLocation);
         telemetry.addData("Driver Direction", driverDirection);
+        telemetry.addData("Robot Offset", robotOffset);
+        telemetry.addData("Robot Heading", robotHeading);
+        telemetry.addData("Robot Direction", robotDirection);
 
         while (opModeInInit()) {
             telemetry.addLine("Press the D-Pad Button towards the audience");
@@ -40,12 +46,35 @@ public class mecanumDriveFO extends LinearOpMode {
                 driverLocation = "Blue Alliance";
             }
 
+            telemetry.addLine("Press the face button in the direction of the robot");
+
+            if (gamepad1.y){
+                robotOffset = 0;
+                robotDirection = "Forward";
+            } else if (gamepad1.b){
+                robotOffset = Math.toRadians(-90);
+                robotDirection = "Right";
+            } else if (gamepad1.a){
+                robotOffset = Math.toRadians(180);
+                robotDirection = "Reverse";
+            } else if (gamepad1.x){
+                robotOffset = Math.toRadians(90);
+                robotDirection = "Left";
+            }
+
+            robotHeading = driverDirection + robotOffset;
+
             telemetry.addData("Driver Location", driverLocation);
             telemetry.addData("Driver Direction", driverDirection);
+            telemetry.addData("Robot Offset", robotOffset);
+            telemetry.addData("Robot Heading", robotHeading);
+            telemetry.addData("Robot Direction", robotDirection);
             telemetry.addLine("ready to go");
             telemetry.update();
         }
         waitForStart();
+
+        drive.pose = new Pose2d(new Vector2d(0,0),robotHeading);
 
         while (opModeIsActive()){
             drive.updatePoseEstimate();
