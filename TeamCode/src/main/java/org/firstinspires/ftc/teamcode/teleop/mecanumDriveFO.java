dcb66779c6e59196bf472e1921fd289373666393
Anay Saran <anaysaran@gmail.com>
2024-11-08 16:37:50 -0800

    Updated the Mecanum DriveFO
    We added the code from SwingArmDrive.java to the mecanumdriveFO.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumDriveFO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/mecanumDriveFO.java
@@ -7,6 +7,10 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Drawing;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
@@ -17,6 +21,33 @@ public class mecanumDriveFO extends LinearOpMode {
     @Override
     public void runOpMode() {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        swingArm armControl = new swingArm();
+
+        DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
+        DcMotorEx actuator = hardwareMap.get(DcMotorEx.class, "actuator");
+        Servo pivot = hardwareMap.get(Servo.class, "pivot");
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        arm.setTargetPosition(0);
+        actuator.setTargetPosition(0);
+
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        arm.setVelocity(armControl.ARM_MAX_VEL);
+        actuator.setVelocity(armControl.ACTUATOR_MAX_VEL);
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        actuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        actuator.setDirection(DcMotorSimple.Direction.FORWARD);
+        pivot.setDirection(Servo.Direction.FORWARD);
+        claw.setDirection(Servo.Direction.FORWARD);
+
 
         double robotOffset = 0;
         String robotDirection = "Forward";
@@ -49,9 +80,49 @@ public class mecanumDriveFO extends LinearOpMode {
         }
         waitForStart();
 
+        double pivotPos = 0.5;
+
         drive.pose = new Pose2d(new Vector2d(0,0),robotOffset);
 
         while (opModeIsActive()){
+
+            if (gamepad1.left_bumper & !gamepad1.right_bumper){
+                armControl.moveClaw(0.0,claw);
+            } else if (gamepad1.right_bumper){
+                armControl.moveClaw(1.0,claw);
+            }
+
+            telemetry.addData("Claw Position", claw.getPosition());
+
+            if (gamepad1.dpad_up ^ gamepad1.dpad_down ^ gamepad1.dpad_right ^ gamepad1.dpad_left) {
+                //on ground
+                if (gamepad1.dpad_down) {
+                    armControl.moveArm(0.0, arm);
+                }
+                //halfway
+                if (gamepad1.dpad_right){
+                    armControl.moveArm(0.5,arm);
+                }
+                if (gamepad1.dpad_left){
+                    armControl.moveArm(0.85,arm);
+                }
+                //slightly backwards
+                if (gamepad1.dpad_up) {
+                    armControl.moveArm(1.0, arm);
+                }
+            }
+
+            telemetry.addData("Arm Position", arm.getCurrentPosition());
+
+            pivotPos = pivotPos + (gamepad1.right_trigger * .10) - (gamepad1.left_trigger * .10);
+
+            //keeps pivotPos between 0 and 1
+            pivotPos = (pivotPos<0) ? 0 : (pivotPos>1) ? 1 : pivotPos;
+
+            armControl.clawPivot(pivotPos,pivot);
+
+            telemetry.addData("Pivot Position", pivot.getPosition());
+
             drive.updatePoseEstimate();
 
             double heading = drive.pose.heading.toDouble();
@@ -93,7 +164,22 @@ public class mecanumDriveFO extends LinearOpMode {
                     robotDirection = "Left";
                     drive.pose = new Pose2d(new Vector2d(0,0),robotOffset);
                 }
+            } else if (gamepad1.a ^ gamepad1.b ^ gamepad1.x ^ gamepad1.y){
+                //fully retracted
+                if (gamepad1.a){
+                    armControl.moveActuator(0.0,actuator);
+                }
+                //half extended
+                if (gamepad1.b || gamepad1.x){
+                    armControl.moveActuator(0.5,actuator);
+                }
+                //fully extended
+                if (gamepad1.y){
+                    armControl.moveActuator(1.0,actuator);
+                }
             }
+
+            telemetry.addData("Actuator Position", actuator.getCurrentPosition());
 
             telemetry.addData("x", drive.pose.position.x);
             telemetry.addData("y", drive.pose.position.y);
