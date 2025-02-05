a65358268d52d99a615b4f43963b7be103eb965d
Tonnieboy300 <tonnieboy300@icloud.com>
2025-02-05 10:13:15 -0800

    add CrServo spinner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/makeCRServoSpinPlease.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/makeCRServoSpinPlease.java
@@ -3,25 +3,26 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Servo Putter Thereer", group="yzz")
+@TeleOp(name="CRServo Spinner", group="yzz")
 @Config
-public class servoPutterThereer extends LinearOpMode {
-    public static String SERVO = "intakeCactus";
-    public static double TARGET_POSITION = 0.5;
+public class makeCRServoSpinPlease extends LinearOpMode {
+    public static String SERVO = "intakeCactusL";
+    public static double POWER = 0.1;
 
     @Override
     public void runOpMode() throws InterruptedException{
-        Servo servo = hardwareMap.get(Servo.class,SERVO);
+        CRServo servo = hardwareMap.get(CRServo.class,SERVO);
 
-        telemetry.addData("Target Position", TARGET_POSITION);
+        telemetry.addData("POWER", POWER);
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()){
-            servo.setPosition(TARGET_POSITION);
+            servo.setPower(POWER);
             telemetry.addLine("Yippee!");
             telemetry.addData("Time Elapsed", getRuntime());
             telemetry.update();
